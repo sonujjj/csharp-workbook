66ab9cbbee4d79473c001c55bd70da6528629d1f
sonujjj <sonujjj@gmail.com>
2017-10-12 18:54:03 -0500

    created single project and updated Hello World

--- 01Week/HelloWorld.cs
+++ 01Week/HelloWorld.cs
@@ -4,7 +4,7 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello!");
-        return;
+        Console.WriteLine("Hello World!");
+        Console.ReadLine();
     }
 }
